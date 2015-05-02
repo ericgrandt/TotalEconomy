e71d279ad35c877aea318a91dbcc3b769a7ddf80
Eric Grandt <3956301+ericgrandt@users.noreply.github.com>
2015-05-02 03:30:06 -0500

    Added the createAccount function.

--- src/com/erigitic/config/AccountManager.java
+++ src/com/erigitic/config/AccountManager.java
@@ -1,9 +1,13 @@
 package com.erigitic.config;
 
 import com.google.inject.Inject;
+import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
+import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.event.entity.player.PlayerJoinEvent;
 import org.spongepowered.api.service.config.DefaultConfig;
 
 import java.io.File;
@@ -20,10 +24,8 @@ public class AccountManager {
     //Load the default config so we grab the parent directory.
 
     private File accountsConfig;
-
-    @Inject
-    @DefaultConfig(sharedRoot = false)
     private ConfigurationLoader<CommentedConfigurationNode> configManager;
+    private ConfigurationNode config = null;
 
     /**
      * Default Constructor so we can access this class from elsewhere
@@ -37,6 +39,7 @@ public class AccountManager {
      */
     public void setupConfig() {
         accountsConfig = new File("config/TotalEconomy/accounts.conf");
+        configManager = HoconConfigurationLoader.builder().setFile(accountsConfig).build();
 
         try {
             if (!accountsConfig.exists()) {
@@ -44,6 +47,21 @@ public class AccountManager {
             }
         } catch (IOException e) {
             logger.warn("ERROR: Could not load/create accounts config file!");
+        }
+    }
+
+    public void createAccount(Player player) {
+        try {
+            config = configManager.load();
+
+            if (config.getNode(player.getName(), "balance").getValue() == null) {
+                //TODO: Set balance to the default config defined starting balance
+                config.getNode(player.getName(), "balance").setValue("0");
+            }
+
+            configManager.save(config);
+        } catch (IOException e) {
+            logger.warn("ERROR: Could not create account!");
         }
     }
 }
