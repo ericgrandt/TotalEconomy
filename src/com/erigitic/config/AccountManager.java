40b03c2e15f710e194854f1e778c351b1efda482
Erigitic <emgrandt@gmail.com>
2015-05-02 02:25:59 -0500

    Added AccountManager.java

---
+++ src/com/erigitic/config/AccountManager.java
@@ -0,0 +1,19 @@
+package com.erigitic.config;
+
+/**
+ * Created by Erigitic on 5/2/2015.
+ */
+public class AccountManager {
+
+    /**
+     * Default Constructor so we can access this class from elsewhere
+     */
+    public AccountManager() { }
+
+    /**
+     * Setup the config file that will contain the user accounts. These accounts will contain the users money amount.
+     */
+    public void setupConfig() {
+
+    }
+}
