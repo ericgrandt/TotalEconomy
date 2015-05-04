af8d6d16c30d8535326f9f40f042aec981580f4b
Eric Grandt <3956301+ericgrandt@users.noreply.github.com>
2015-05-04 18:01:22 -0500

    Balance command added

---
+++ src/com/erigitic/commands/BalanceCommand.java
@@ -0,0 +1,38 @@
+package com.erigitic.commands;
+
+import com.erigitic.config.AccountManager;
+import com.erigitic.main.TotalEconomy;
+import org.slf4j.Logger;
+import org.spongepowered.api.entity.player.Player;
+import org.spongepowered.api.text.Texts;
+import org.spongepowered.api.util.command.CommandException;
+import org.spongepowered.api.util.command.CommandResult;
+import org.spongepowered.api.util.command.CommandSource;
+import org.spongepowered.api.util.command.args.CommandContext;
+import org.spongepowered.api.util.command.spec.CommandExecutor;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Erigitic on 5/4/2015.
+ */
+public class BalanceCommand implements CommandExecutor {
+    private Logger logger;
+    private TotalEconomy totalEconomy;
+    private AccountManager accountManager;
+
+    public BalanceCommand(TotalEconomy totalEconomy) {
+        this.totalEconomy = totalEconomy;
+        logger = totalEconomy.getLogger();
+
+        accountManager = totalEconomy.getAccountManager();
+    }
+
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        Player sender = ((Player) src).getPlayer().get();
+
+        sender.sendMessage(Texts.of("Balance: " + accountManager.getStringBalance(sender)));
+
+        return CommandResult.success();
+    }
+}
