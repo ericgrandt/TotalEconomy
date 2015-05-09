7d0aaf9bd4e20fd144c425a3e623e4371ef5f731
Erigitic <emgrandt@gmail.com>
2015-05-09 01:57:00 -0500

    More config stuff

--- src/com/erigitic/jobs/TEJobs.java
+++ src/com/erigitic/jobs/TEJobs.java
@@ -3,9 +3,12 @@ package com.erigitic.jobs;
 import com.erigitic.config.AccountManager;
 import com.erigitic.main.TotalEconomy;
 import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.ConfigurationOptions;
+import ninja.leaping.configurate.SimpleConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.util.MapFactories;
 import org.slf4j.Logger;
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.text.Texts;
@@ -23,8 +26,8 @@ public class TEJobs {
     private ConfigurationNode accountConfig;
     private Logger logger;
 
-    private File jobsFile; //Will contain all the jobs with salaries, tasks(Pay, xp reward), jobName, and etc.
-    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+    private File jobsFile; //Will contain all the jobs with salaries, jobName, and etc.
+    private ConfigurationLoader<CommentedConfigurationNode> loader;
     private ConfigurationNode jobsConfig;
 
     public TEJobs(TotalEconomy totalEconomy) {
@@ -35,10 +38,10 @@ public class TEJobs {
         logger = totalEconomy.getLogger();
 
         jobsFile = new File(totalEconomy.getConfigDir(), "jobs.conf");
-        configManager = HoconConfigurationLoader.builder().setFile(jobsFile).build();
+        loader = HoconConfigurationLoader.builder().setFile(jobsFile).build();
 
         try {
-            jobsConfig = configManager.load();
+            jobsConfig = loader.load();
         } catch (IOException e) {
             logger.warn("Could not load jobs config!");
         }
@@ -50,21 +53,14 @@ public class TEJobs {
                 jobsFile.createNewFile();
 
                 jobsConfig.getNode("jobs").setValue("Miner, Lumberjack");
-                jobsConfig.getNode("Miner", "salary").setValue("25");
-                jobsConfig.getNode("Lumberjack", "salary").setValue("25");
-                configManager.save(jobsConfig);
-            }
+                jobsConfig.getNode("Miner", "basepay").setValue("1");
+                jobsConfig.getNode("Miner", "experience", "coal_ore").setValue("4");
+                jobsConfig.getNode("Lumberjack", "basepay").setValue("1");
+                jobsConfig.getNode("Lumberjack", "experience", "item1").setValue("4");
 
-            if (jobsConfig.getNode("jobs").getValue() == null) {
-                jobsConfig.getNode("jobs").setValue("Miner, Lumberjack");
-            }
+                setupRanks();
 
-            if (jobsConfig.getNode("Miner", "salary").getValue() == null) {
-                jobsConfig.getNode("Miner", "salary").setValue("25");
-            }
-
-            if (jobsConfig.getNode("Lumberjack", "salary").getValue() == null) {
-                jobsConfig.getNode("Lumberjack", "salary").setValue("25");
+                loader.save(jobsConfig);
             }
         } catch (IOException e) {
             logger.warn("Could not create jobs config file!");
@@ -72,14 +68,14 @@ public class TEJobs {
     }
 
     /**
-     * Pays the player their salary on a timed basis. This time can be changed in config(TODO).
-     *
-     * NOTE: Not sure if I want to have a salary or not. Will decide later. Currently this function will be empty.
-     *
-     * @param jobName (MAY NOT BE NEEDED) name of the job
+     * Setup the job ranks
      */
-    public void paySalary(String jobName) {
-
+    private void setupRanks() {
+        jobsConfig.getNode("ranks", "novice").setValue("1");
+        jobsConfig.getNode("ranks", "advanced").setValue("25");
+        jobsConfig.getNode("ranks", "proficient").setValue("50");
+        jobsConfig.getNode("ranks", "expert").setValue("150");
+        jobsConfig.getNode("ranks", "master").setValue("300");
     }
 
     /**
@@ -135,5 +131,10 @@ public class TEJobs {
         }
 
         return false;
+    }
+
+    public String[] getTasks() {
+
+        return null;
     }
 }
