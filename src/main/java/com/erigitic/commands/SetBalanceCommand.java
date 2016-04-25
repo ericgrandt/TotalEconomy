0ba54aaf5a7873794981ded05227d1e9ee59b881
Magnus (aka. mark332 aka. markolos aka. Life4YourGames aka. LiveYourGames) ;) <contact@life4yourgames.de.vu>
2016-04-25 17:07:05 +0200

    Resolved exception when running from Console/CommandBlock
    - Resolves #57
    - Tried to add a way to check if the player is online -> Turns out
    'isOnline' does not work if you directly get the player from the
    optional (exception is thrown earlier)

--- src/main/java/com/erigitic/commands/SetBalanceCommand.java
+++ src/main/java/com/erigitic/commands/SetBalanceCommand.java
@@ -7,6 +7,8 @@ import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.source.CommandBlockSource;
+import org.spongepowered.api.command.source.ConsoleSource;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.cause.Cause;
@@ -28,16 +30,39 @@ public class SetBalanceCommand implements CommandExecutor {
 
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        Player sender = ((Player) src).getPlayer().get();
+        //Declare response for context
+        Text response = Text.of("TE: Report an issue on GitHub if you get to see this!");
+
         Player recipient = (Player) args.getOne("player").get();
-        BigDecimal amount = new BigDecimal((String) args.getOne("amount").get()).setScale(2, BigDecimal.ROUND_DOWN);
-        Text symbol = accountManager.getDefaultCurrency().getSymbol();
+        //Wish this would've worked, but it doesn't...
+        if (recipient.isOnline()) {
+            //Player online -> Change balance
+            BigDecimal amount = new BigDecimal((String) args.getOne("amount").get()).setScale(2, BigDecimal.ROUND_DOWN);
+            Text symbol = accountManager.getDefaultCurrency().getSymbol();
 
-        TEAccount recipientAccount = (TEAccount) accountManager.getOrCreateAccount(recipient.getUniqueId()).get();
+            TEAccount recipientAccount = (TEAccount) accountManager.getOrCreateAccount(recipient.getUniqueId()).get();
 
-        recipientAccount.setBalance(accountManager.getDefaultCurrency(), amount, Cause.of(NamedCause.of("TotalEconomy", this)));
+            recipientAccount.setBalance(accountManager.getDefaultCurrency(), amount, Cause.of(NamedCause.of("TotalEconomy", this)));
 
-        sender.sendMessage(Text.of(TextColors.GRAY, "You set ", recipient.getName(), "\'s balance to ", TextColors.GOLD, symbol, amount));
+            //Format/Set reply:
+            response = Text.of(TextColors.GRAY, "You set ", recipient.getName(), "\'s balance to ", TextColors.GOLD, symbol, amount);
+        } else {
+            //TODO:Implement a way of accessing accounts of offline players
+            //Player NOT online: Show an error
+            response = Text.of(TextColors.RED, "Player ", args.getOne("player"), " not found!");
+        }
+
+        //Fixes: #57
+        if (src instanceof Player) {
+            //Sent from player -> reply to player
+            ((Player) src).getPlayer().get().sendMessage(response);
+        } else if (src instanceof ConsoleSource) {
+            //Sent from console -> reply to console
+            ((ConsoleSource) src).sendMessage(response);
+        } else if (src instanceof CommandBlockSource) {
+            //Sent from commandBlock -> reply to commandBlock
+            ((CommandBlockSource) src).sendMessage(response);
+        }
 
         return CommandResult.success();
     }
