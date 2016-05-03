85b11fbca4e0bff322a0e114f73eb0e7a243d835
Erigitic <emgrandt@gmail.com>
2016-05-03 17:41:45 -0600

    Finalized v1.4.1

--- src/main/java/com/erigitic/main/TotalEconomy.java
+++ src/main/java/com/erigitic/main/TotalEconomy.java
@@ -31,7 +31,7 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
 
-@Plugin(id = "totaleconomy", name = "Total Economy", version = "1.4.0")
+@Plugin(id = "totaleconomy", name = "Total Economy", version = "1.4.1")
 public class TotalEconomy {
 
     @Inject
