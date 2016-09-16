63c01df5a1a4d8247caa169ca424a90d668ea914
Eric Grandt <3956301+ericgrandt@users.noreply.github.com>
2016-09-16 15:59:29 -0500

    Fixes #94

--- src/main/java/com/erigitic/main/TotalEconomy.java
+++ src/main/java/com/erigitic/main/TotalEconomy.java
@@ -57,7 +57,7 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
 
-@Plugin(id = "totaleconomy", name = "Total Economy", version = "1.5.0", description = "All in one economy plugin for Minecraft/Sponge")
+@Plugin(id = "totaleconomy", name = "Total Economy", version = "1.5.1", description = "All in one economy plugin for Minecraft/Sponge")
 public class TotalEconomy {
 
     @Inject
