ee2b1ed8d8ad8388c66c5c3fd8a8213eaefa4051
Eric <emgrandt@gmail.com>
2017-08-04 16:27:54 -0500

    Update version number

--- src/main/java/com/erigitic/main/TotalEconomy.java
+++ src/main/java/com/erigitic/main/TotalEconomy.java
@@ -60,7 +60,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.*;
 
-@Plugin(id = "totaleconomy", name = "Total Economy", version = "1.7.0", description = "All in one economy plugin for Minecraft/Sponge")
+@Plugin(id = "totaleconomy", name = "Total Economy", version = "1.7.1", description = "All in one economy plugin for Minecraft/Sponge")
 public class TotalEconomy {
 
     @Inject
