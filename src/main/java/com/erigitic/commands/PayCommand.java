90da57fc41b0af8e696b5da1345d0066735e6803
Eric <Erigitic@users.noreply.github.com>
2020-04-14 20:18:05 -0500

    Create PayCommand.java

---
+++ src/main/java/com/erigitic/commands/PayCommand.java
@@ -0,0 +1,14 @@
+package com.erigitic.commands;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+
+public class PayCommand implements CommandExecutor {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        return null;
+    }
+}
