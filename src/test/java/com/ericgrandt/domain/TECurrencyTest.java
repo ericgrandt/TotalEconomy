f8f97615e558467567a827d0a208f1aa12cc09cf
Eric <3956301+ericgrandt@users.noreply.github.com>
2021-07-22 22:41:51 -0500

    More tests

--- src/test/java/com/ericgrandt/domain/TECurrencyTest.java
+++ src/test/java/com/ericgrandt/domain/TECurrencyTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
