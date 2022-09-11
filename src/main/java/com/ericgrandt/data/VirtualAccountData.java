bae0f3bffed7d010741dadf13631f002a56f0702
Eric <3956301+ericgrandt@users.noreply.github.com>
2022-09-10 21:12:16 -0500

    Add delete virtual account and split account and virtual account

--- src/main/java/com/ericgrandt/data/VirtualAccountData.java
+++ src/main/java/com/ericgrandt/data/VirtualAccountData.java
@@ -1,6 +1,5 @@
 package com.ericgrandt.data;
 
-import com.ericgrandt.data.dto.AccountDto;
 import com.ericgrandt.data.dto.VirtualAccountDto;
 import org.apache.logging.log4j.Logger;
 
@@ -8,34 +7,33 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.UUID;
 
-public class AccountData {
+public class VirtualAccountData {
     private final Logger logger;
     private final Database database;
 
-    public AccountData(Logger logger, Database database) {
+    public VirtualAccountData(Logger logger, Database database) {
         this.logger = logger;
         this.database = database;
     }
 
-    public boolean createAccount(UUID accountId) {
-        String createAccountQuery = "INSERT INTO te_account(id) VALUES (?)";
+    public boolean createVirtualAccount(String identifier) {
+        String createVirtualAccountQuery = "INSERT INTO te_virtual_account(identifier) VALUES (?)";
 
         try (
             Connection conn = database.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(createAccountQuery)
+            PreparedStatement stmt = conn.prepareStatement(createVirtualAccountQuery)
         ) {
-            stmt.setString(1, accountId.toString());
+            stmt.setString(1, identifier);
             stmt.execute();
 
             return true;
         } catch (SQLException e) {
             logger.error(
                 String.format(
-                    "Error creating account (Query: %s, Parameters: %s)",
-                    createAccountQuery,
-                    accountId
+                    "Error creating virtual account (Query: %s, Parameters: %s)",
+                    createVirtualAccountQuery,
+                    identifier
                 ),
                 e
             );
@@ -44,19 +42,20 @@ public class AccountData {
         return false;
     }
 
-    public AccountDto getAccount(UUID accountId) {
-        String getAccountQuery = "SELECT * FROM te_account WHERE id = ?";
+    public VirtualAccountDto getVirtualAccount(String identifier) {
+        String getVirtualAccountQuery = "SELECT * FROM te_virtual_account WHERE identifier = ?";
 
         try (
             Connection conn = database.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(getAccountQuery)
+            PreparedStatement stmt = conn.prepareStatement(getVirtualAccountQuery)
         ) {
-            stmt.setString(1, accountId.toString());
+            stmt.setString(1, identifier);
 
             try (ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
-                    return new AccountDto(
+                    return new VirtualAccountDto(
                         rs.getString("id"),
+                        rs.getString("identifier"),
                         rs.getTimestamp("created")
                     );
                 }
@@ -64,9 +63,9 @@ public class AccountData {
         } catch (SQLException e) {
             logger.error(
                 String.format(
-                    "Error getting account (Query: %s, Parameters: %s)",
-                    getAccountQuery,
-                    accountId
+                    "Error getting virtual account (Query: %s, Parameters: %s)",
+                    getVirtualAccountQuery,
+                    identifier
                 ),
                 e
             );
@@ -75,23 +74,23 @@ public class AccountData {
         return null;
     }
 
-    public boolean deleteAccount(UUID accountId) {
-        String deleteAccountQuery = "DELETE FROM te_account WHERE id = ?";
+    public boolean deleteVirtualAccount(String identifier) {
+        String deleteVirtualAccountQuery = "DELETE FROM te_virtual_account WHERE identifier = ?";
 
         try (
             Connection conn = database.getConnection();
-            PreparedStatement stmt = conn.prepareStatement(deleteAccountQuery)
+            PreparedStatement stmt = conn.prepareStatement(deleteVirtualAccountQuery)
         ) {
-            stmt.setString(1, accountId.toString());
+            stmt.setString(1, identifier);
             int rowsDeleted = stmt.executeUpdate();
 
             return rowsDeleted != 0;
         } catch (SQLException e) {
             logger.error(
                 String.format(
-                    "Error deleting account (Query: %s, Parameters: %s)",
-                    deleteAccountQuery,
-                    accountId
+                    "Error deleting virtual account (Query: %s, Parameters: %s)",
+                    deleteVirtualAccountQuery,
+                    identifier
                 ),
                 e
             );
