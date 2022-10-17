940ed9b1f391d1515e164b4175fd5d796317cf70
Eric <3956301+ericgrandt@users.noreply.github.com>
2022-10-16 22:07:57 -0500

    Update CurrencyDataTest.java

--- src/test/java/com/ericgrandt/data/CurrencyDataTest.java
+++ src/test/java/com/ericgrandt/data/CurrencyDataTest.java
@@ -17,7 +17,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-
 @ExtendWith(MockitoExtension.class)
 public class CurrencyDataTest {
     @Test
