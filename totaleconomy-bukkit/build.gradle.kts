90c13cb5a4ef900356756d03b1e972a4e16066ad
Eric <3956301+ericgrandt@users.noreply.github.com>
2024-01-06 09:58:46 -0600

    Group imports in a more organized way

--- totaleconomy-bukkit/build.gradle.kts
+++ totaleconomy-bukkit/build.gradle.kts
@@ -11,28 +11,25 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":totaleconomy-common"))
-
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-    testImplementation("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
-
     compileOnly("com.github.MilkBowl:VaultAPI:1.7")
-    testImplementation("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
 
+    implementation(project(":totaleconomy-common"))
     implementation("com.zaxxer:HikariCP:5.0.1") {
         exclude("org.slf4j", "slf4j-api")
     }
-    testImplementation("com.zaxxer:HikariCP:5.0.1")
+    implementation("org.mybatis:mybatis:3.5.11")
+
+    testImplementation("com.github.MilkBowl:VaultAPI:1.7")
     testImplementation("com.h2database:h2:2.2.222")
-
+    testImplementation("com.zaxxer:HikariCP:5.0.1")
+    testImplementation("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-
     testImplementation("org.mockito:mockito-core:4.8.0")
     testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
-
-    implementation("org.mybatis:mybatis:3.5.11")
     testImplementation("org.mybatis:mybatis:3.5.11")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 java {
