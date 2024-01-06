76ba71453f85a0d163748b0ba4507108d2d5ec11
Eric <3956301+ericgrandt@users.noreply.github.com>
2024-01-05 23:31:37 -0600

    Checkpoint

--- totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/TotalEconomy.java
+++ totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/TotalEconomy.java
@@ -1,22 +1,15 @@
 package com.ericgrandt.totaleconomy;
 
 import com.google.inject.Inject;
-import net.kyori.adventure.identity.Identity;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.LinearComponents;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.format.Style;
-import net.kyori.adventure.text.format.TextDecoration;
+import java.net.URL;
 import org.apache.logging.log4j.Logger;
-import org.spongepowered.api.Server;
-import org.spongepowered.api.command.Command;
-import org.spongepowered.api.command.CommandResult;
-import org.spongepowered.api.command.parameter.Parameter;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.lifecycle.ConstructPluginEvent;
-import org.spongepowered.api.event.lifecycle.RegisterCommandEvent;
-import org.spongepowered.api.event.lifecycle.StartingEngineEvent;
-import org.spongepowered.api.event.lifecycle.StoppingEngineEvent;
+import org.spongepowered.configurate.CommentedConfigurationNode;
+import org.spongepowered.configurate.ConfigurateException;
+import org.spongepowered.configurate.ConfigurationNode;
+import org.spongepowered.configurate.loader.ConfigurationLoader;
+import org.spongepowered.configurate.yaml.YamlConfigurationLoader;
 import org.spongepowered.plugin.PluginContainer;
 import org.spongepowered.plugin.builtin.jvm.Plugin;
 
@@ -24,6 +17,8 @@ import org.spongepowered.plugin.builtin.jvm.Plugin;
 public class TotalEconomy {
     private final PluginContainer container;
     private final Logger logger;
+
+    private ConfigurationLoader<CommentedConfigurationNode> loader;
 
     @Inject
     private TotalEconomy(final PluginContainer container, final Logger logger) {
@@ -33,41 +28,17 @@ public class TotalEconomy {
 
     @Listener
     public void onConstructPlugin(final ConstructPluginEvent event) {
-        // Perform any one-time setup
+        final URL configUrl = this.getClass().getResource("/config.yml");
+        loader = YamlConfigurationLoader
+            .builder()
+            .url(configUrl)
+            .build();
+        try {
+            ConfigurationNode rootNode = loader.load();
+            logger.info(rootNode.node("database", "url").getString());
+        } catch (ConfigurateException e) {
+            throw new RuntimeException(e);
+        }
         this.logger.info("Constructing totaleconomy");
-    }
-
-    @Listener
-    public void onServerStarting(final StartingEngineEvent<Server> event) {
-        // Any setup per-game instance. This can run multiple times when
-        // using the integrated (singleplayer) server.
-    }
-
-    @Listener
-    public void onServerStopping(final StoppingEngineEvent<Server> event) {
-        // Any tear down per-game instance. This can run multiple times when
-        // using the integrated (singleplayer) server.
-    }
-
-    @Listener
-    public void onRegisterCommands(final RegisterCommandEvent<Command.Parameterized> event) {
-        // Register a simple command
-        // When possible, all commands should be registered within a command register event
-        final Parameter.Value<String> nameParam = Parameter.string().key("name").build();
-        event.register(this.container, Command.builder()
-            .addParameter(nameParam)
-            .permission("totaleconomy.command.greet")
-            .executor(ctx -> {
-                final String name = ctx.requireOne(nameParam);
-                ctx.sendMessage(Identity.nil(), LinearComponents.linear(
-                    NamedTextColor.AQUA,
-                    Component.text("Hello "),
-                    Component.text(name, Style.style(TextDecoration.BOLD)),
-                    Component.text("!")
-                ));
-
-                return CommandResult.success();
-            })
-            .build(), "greet", "wave");
     }
 }
