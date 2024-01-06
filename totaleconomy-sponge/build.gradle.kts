d5528a5f800b608ac153627c03260872d3c366b9
Eric <3956301+ericgrandt@users.noreply.github.com>
2024-01-05 20:47:34 -0600

    Move totaleconomy-bukkit to it's own folder and add sponge skeleton

---
+++ totaleconomy-sponge/build.gradle.kts
@@ -0,0 +1,61 @@
+import org.spongepowered.gradle.plugin.config.PluginLoaders
+import org.spongepowered.plugin.metadata.model.PluginDependency
+
+plugins {
+    `java-library`
+    id("org.spongepowered.gradle.plugin") version "2.1.1"
+}
+
+group = "com.ericgrandt"
+version = "2.0"
+
+repositories {
+    mavenCentral()
+}
+
+sponge {
+    apiVersion("8.1.0")
+    license("MIT")
+    loader {
+        name(PluginLoaders.JAVA_PLAIN)
+        version("1.0")
+    }
+    plugin("totaleconomy") {
+        displayName("Total Economy")
+        entrypoint("com.ericgrandt.totaleconomy.TotalEconomy")
+        description("My plugin description")
+        links {
+            // homepage("https://spongepowered.org")
+            // source("https://spongepowered.org/source")
+            // issues("https://spongepowered.org/issues")
+        }
+        dependency("spongeapi") {
+            loadOrder(PluginDependency.LoadOrder.AFTER)
+            optional(false)
+        }
+    }
+}
+
+val javaTarget = 8 // Sponge targets a minimum of Java 8
+java {
+    sourceCompatibility = JavaVersion.toVersion(javaTarget)
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+    if (JavaVersion.current() < JavaVersion.toVersion(javaTarget)) {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(javaTarget))
+    }
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        if (JavaVersion.current().isJava10Compatible) {
+            release.set(javaTarget)
+        }
+    }
+}
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType(AbstractArchiveTask::class).configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
+}
