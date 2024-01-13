4344cfddbcd1363bc43ef30fd9555ecf2deec1c0
Eric <3956301+ericgrandt@users.noreply.github.com>
2024-01-12 22:15:34 -0600

    Add TransactionResultImpl and tests

---
+++ totaleconomy-sponge/src/test/java/com/ericgrandt/totaleconomy/helpers/TransactionTypeMock.java
@@ -0,0 +1,7 @@
+package com.ericgrandt.totaleconomy.helpers;
+
+import org.spongepowered.api.service.economy.transaction.TransactionType;
+
+public class TransactionTypeMock implements TransactionType {
+
+}
