1debcce94deb229e076ec9fac0cbdace8fe68a19
Eric <3956301+ericgrandt@users.noreply.github.com>
2024-02-26 20:06:22 -0600

    Start CommonEconomy implementation

--- totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/econ/CommonEconomy.java
+++ totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/econ/CommonEconomy.java
@@ -1,10 +1,69 @@
 package com.ericgrandt.totaleconomy.common.econ;
 
-import com.ericgrandt.totaleconomy.common.game.CommonPlayer;
-import net.kyori.adventure.text.Component;
+import com.ericgrandt.totaleconomy.common.data.AccountData;
+import com.ericgrandt.totaleconomy.common.data.BalanceData;
+import com.ericgrandt.totaleconomy.common.data.CurrencyData;
+import com.ericgrandt.totaleconomy.common.data.dto.CurrencyDto;
+import com.ericgrandt.totaleconomy.common.logger.CommonLogger;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.UUID;
 
-public interface CommonEconomy {
-    double getBalance(CommonPlayer player);
+public class CommonEconomy {
+    private final CommonLogger logger;
+    private final AccountData accountData;
+    private final BalanceData balanceData;
+    private final CurrencyData currencyData;
 
-    Component formatBalance(double balance);
+    public CommonEconomy(
+        final CommonLogger logger,
+        final AccountData accountData,
+        final BalanceData balanceData,
+        final CurrencyData currencyData
+    ) {
+        this.logger = logger;
+        this.accountData = accountData;
+        this.balanceData = balanceData;
+        this.currencyData = currencyData;
+    }
+
+    public CurrencyDto getDefaultCurrency() {
+        try {
+            return currencyData.getDefaultCurrency();
+        } catch (SQLException e) {
+            logger.error(
+                "[Total Economy] Error calling getDefaultCurrency",
+                e
+            );
+            return null;
+        }
+    }
+
+    public boolean hasAccount(UUID uuid) {
+        try {
+            return accountData.getAccount(uuid) != null;
+        } catch (SQLException e) {
+            logger.error(
+                String.format("[Total Economy] Error calling getAccount (accountId: %s)", uuid),
+                e
+            );
+            return false;
+        }
+    }
+
+    public BigDecimal getBalance(UUID uuid, int currencyId) {
+        try {
+            return balanceData.getBalance(uuid, currencyId);
+        } catch (SQLException e) {
+            logger.error(
+                String.format(
+                    "[Total Economy] Error calling getBalance (accountId: %s, currencyId: %s)",
+                    uuid,
+                    currencyId
+                ),
+                e
+            );
+            return null;
+        }
+    }
 }
---
+++ totaleconomy-common/src/test/java/com/ericgrandt/totaleconomy/common/econ/CommonEconomyTest.java
@@ -0,0 +1,172 @@
+package com.ericgrandt.totaleconomy.common.econ;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.ericgrandt.totaleconomy.common.data.AccountData;
+import com.ericgrandt.totaleconomy.common.data.BalanceData;
+import com.ericgrandt.totaleconomy.common.data.CurrencyData;
+import com.ericgrandt.totaleconomy.common.data.dto.AccountDto;
+import com.ericgrandt.totaleconomy.common.data.dto.CurrencyDto;
+import com.ericgrandt.totaleconomy.common.logger.CommonLogger;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.UUID;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CommonEconomyTest {
+    @Mock
+    private CommonLogger loggerMock;
+
+    @Mock
+    private AccountData accountDataMock;
+
+    @Mock
+    private BalanceData balanceDataMock;
+
+    @Mock
+    private CurrencyData currencyDataMock;
+
+    @Test
+    @Tag("Unit")
+    public void getDefaultCurrency_WithDefaultCurrency_ShouldReturnCurrency() throws SQLException {
+        // Arrange
+        CurrencyDto defaultCurrency = new CurrencyDto(
+            1,
+            "singular",
+            "plural",
+            "$",
+            2,
+            true
+        );
+
+        when(currencyDataMock.getDefaultCurrency()).thenReturn(defaultCurrency);
+
+        CommonEconomy sut = new CommonEconomy(loggerMock, accountDataMock, balanceDataMock, currencyDataMock);
+
+        // Act
+        CurrencyDto actual = sut.getDefaultCurrency();
+        CurrencyDto expected = defaultCurrency;
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Tag("Unit")
+    public void getDefaultCurrency_WithSqlException_ShouldReturnNull() throws SQLException {
+        // Arrange
+        when(currencyDataMock.getDefaultCurrency()).thenThrow(SQLException.class);
+
+        CommonEconomy sut = new CommonEconomy(loggerMock, accountDataMock, balanceDataMock, currencyDataMock);
+
+        // Act
+        CurrencyDto actual = sut.getDefaultCurrency();
+
+        // Assert
+        assertNull(actual);
+        verify(loggerMock, times(1)).error(any(String.class), any(SQLException.class));
+    }
+
+    @Test
+    @Tag("Unit")
+    public void hasAccount_WithAccount_ShouldReturnTrue() throws SQLException {
+        // Arrange
+        UUID uuid = UUID.randomUUID();
+        AccountDto account = new AccountDto(uuid.toString(), null);
+
+        when(accountDataMock.getAccount(uuid)).thenReturn(account);
+
+        CommonEconomy sut = new CommonEconomy(loggerMock, accountDataMock, balanceDataMock, currencyDataMock);
+
+        // Act
+        boolean actual = sut.hasAccount(uuid);
+
+        // Assert
+        assertTrue(actual);
+    }
+
+    @Test
+    @Tag("Unit")
+    public void hasAccount_WithNoAccount_ShouldReturnFalse() throws SQLException {
+        // Arrange
+        UUID uuid = UUID.randomUUID();
+
+        when(accountDataMock.getAccount(uuid)).thenReturn(null);
+
+        CommonEconomy sut = new CommonEconomy(loggerMock, accountDataMock, balanceDataMock, currencyDataMock);
+
+        // Act
+        boolean actual = sut.hasAccount(uuid);
+
+        // Assert
+        assertFalse(actual);
+    }
+
+    @Test
+    @Tag("Unit")
+    public void hasAccount_WithSqlException_ShouldReturnFalse() throws SQLException {
+        // Arrange
+        UUID uuid = UUID.randomUUID();
+
+        when(accountDataMock.getAccount(uuid)).thenThrow(SQLException.class);
+
+        CommonEconomy sut = new CommonEconomy(loggerMock, accountDataMock, balanceDataMock, currencyDataMock);
+
+        // Act
+        boolean actual = sut.hasAccount(uuid);
+
+        // Assert
+        assertFalse(actual);
+        verify(loggerMock, times(1)).error(any(String.class), any(SQLException.class));
+    }
+
+    @Test
+    @Tag("Unit")
+    public void getBalance_WithBalance_ShouldReturnBalance() throws SQLException {
+        // Arrange
+        UUID uuid = UUID.randomUUID();
+        int currencyId = 1;
+
+        when(balanceDataMock.getBalance(uuid, currencyId)).thenReturn(BigDecimal.TEN);
+
+        CommonEconomy sut = new CommonEconomy(loggerMock, accountDataMock, balanceDataMock, currencyDataMock);
+
+        // Act
+        BigDecimal actual = sut.getBalance(uuid, currencyId);
+        BigDecimal expected = BigDecimal.TEN;
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @Tag("Unit")
+    public void getBalance_WithSqlException_ShouldReturnNull() throws SQLException {
+        // Arrange
+        UUID uuid = UUID.randomUUID();
+        int currencyId = 1;
+
+        when(balanceDataMock.getBalance(uuid, currencyId)).thenThrow(SQLException.class);
+
+        CommonEconomy sut = new CommonEconomy(loggerMock, accountDataMock, balanceDataMock, currencyDataMock);
+
+        // Act
+        BigDecimal actual = sut.getBalance(uuid, currencyId);
+
+        // Assert
+        assertNull(actual);
+        verify(loggerMock, times(1)).error(any(String.class), any(SQLException.class));
+    }
+}
