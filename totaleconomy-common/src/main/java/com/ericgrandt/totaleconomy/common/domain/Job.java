45744512ec676902563f57f9149a7a17111bc7eb
Eric <3956301+ericgrandt@users.noreply.github.com>
2024-05-12 17:34:28 -0500

    Checkpoint

---
+++ totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/domain/Job.java
@@ -0,0 +1,11 @@
+package com.ericgrandt.totaleconomy.common.domain;
+
+public class Job {
+    private final String id;
+    private final String jobName;
+
+    public Job(final String id, final String jobName) {
+        this.id = id;
+        this.jobName = jobName;
+    }
+}
---
+++ totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/listeners/CommonJobListener.java
@@ -0,0 +1,19 @@
+package com.ericgrandt.totaleconomy.common.listeners;
+
+import com.ericgrandt.totaleconomy.common.econ.CommonEconomy;
+
+public class CommonJobListener {
+//    private final CommonEconomy economy;
+//    private final JobService jobService;
+//    private final int currencyId;
+//
+//    public JobListener(
+//        final CommonEconomy economy,
+//        final JobService jobService,
+//        final int currencyId
+//    ) {
+//        this.economy = economy;
+//        this.jobService = jobService;
+//        this.currencyId = currencyId;
+//    }
+}
