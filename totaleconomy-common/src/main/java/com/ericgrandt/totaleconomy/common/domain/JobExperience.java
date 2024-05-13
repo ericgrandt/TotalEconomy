b644cdc8dbbd07c0c8a7422cc809fa8e9a27066a
Eric <3956301+ericgrandt@users.noreply.github.com>
2024-05-12 20:06:44 -0500

    Checkpoint

--- totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/domain/JobExperience.java
+++ totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/domain/JobExperience.java
@@ -17,5 +17,24 @@ public class JobExperience {
         this.jobId = jobId;
         this.experience = experience;
     }
+
+    public int level() {
+        int level = (int) Math.ceil(Math.sqrt(experience) / 7);
+        return Math.max(level, 1);
+    }
+
+    public int nextLevelExperience() {
+        return (int) Math.ceil(49 * Math.pow(level(), 2)) + 1;
+    }
+
+    public boolean willLevelUp(int experienceToAdd) {
+        return false;
+    }
+//
+//
+//    public int currentLevelBaseExperience() {
+//        int experience = (int) Math.ceil(49 * Math.pow(level() - 1, 2));
+//        return experience > 0 ? experience + 1 : experience;
+//    }
 }
 
