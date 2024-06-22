06e5a2afec80c3b03408a19b629cfe51ca8af7b5
Eric Grandt <emgrandt@gmail.com>
2024-06-21 21:37:20 -0500

    Updates

--- totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/game/CommonSender.java
+++ totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/game/CommonSender.java
@@ -1,8 +1,7 @@
 package com.ericgrandt.totaleconomy.common.game;
 
 import net.kyori.adventure.text.Component;
-import org.jetbrains.annotations.NotNull;
 
 public interface CommonSender {
-    void sendMessage(@NotNull Component message);
+    void sendMessage(Component message);
 }
--- totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/commonimpl/SpongePlayer.java
+++ totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/commonimpl/SpongePlayer.java
@@ -3,12 +3,11 @@ package com.ericgrandt.totaleconomy.commonimpl;
 import com.ericgrandt.totaleconomy.common.game.CommonPlayer;
 import java.util.UUID;
 import net.kyori.adventure.text.Component;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
 
 public record SpongePlayer(ServerPlayer player) implements CommonPlayer {
     @Override
-    public void sendMessage(@NotNull Component message) {
+    public void sendMessage(Component message) {
         player.sendMessage(message);
     }
 
