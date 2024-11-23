36beebae33eb548c1ccf3f7d80e05c4a57a4b54d
Eric Grandt <3956301+ericgrandt@users.noreply.github.com>
2024-11-23 15:59:36 -0600

    Add experience bar class and logic

--- totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/jobs/ExperienceBar.java
+++ totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/jobs/ExperienceBar.java
@@ -1,5 +1,6 @@
 package com.ericgrandt.totaleconomy.common.jobs;
 
+import com.ericgrandt.totaleconomy.common.dto.ExperienceBarDto;
 import com.ericgrandt.totaleconomy.common.game.CommonPlayer;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -9,25 +10,49 @@ import net.kyori.adventure.text.Component;
 
 public class ExperienceBar {
     private final CommonPlayer player;
+    private final ScheduledThreadPoolExecutor scheduler;
     private final BossBar bossBar;
 
-    private ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(1);
     private ScheduledFuture<?> task;
 
-    public ExperienceBar(final CommonPlayer player) {
+    public ExperienceBar(final CommonPlayer player, final ScheduledThreadPoolExecutor scheduler) {
         this.player = player;
+        this.scheduler = scheduler;
         this.bossBar = BossBar.bossBar(Component.empty(), 0.0f, BossBar.Color.BLUE, BossBar.Overlay.PROGRESS);
     }
 
-    public void show() {
+    public void show(ExperienceBarDto expBarDto, int expGain) {
         if (task != null) {
             task.cancel(true);
         }
-        task = scheduler.schedule(this::hide, 5, TimeUnit.SECONDS);
+
+        setExpBarTitle(expBarDto, expGain);
+        setProgress(expBarDto);
         player.showBossBar(bossBar);
+        task = scheduler.schedule(this::hide, 5, TimeUnit.SECONDS);
     }
 
     public void hide() {
         player.hideBossBar(bossBar);
+    }
+
+    private void setExpBarTitle(ExperienceBarDto expBarDto, int expGain) {
+        bossBar.name(
+            Component.text(
+                String.format(
+                    "%s [+%s EXP] [%s/%s]",
+                    expBarDto.jobName(),
+                    expGain,
+                    expBarDto.exp(),
+                    expBarDto.expToNext()
+                )
+            )
+        );
+    }
+
+    private void setProgress(ExperienceBarDto expBarDto) {
+        float normalizedStart = expBarDto.exp() - expBarDto.levelBaseExp();
+        float normalizedEnd = expBarDto.expToNext() - expBarDto.levelBaseExp();
+        this.bossBar.progress(normalizedStart / normalizedEnd);
     }
 }
