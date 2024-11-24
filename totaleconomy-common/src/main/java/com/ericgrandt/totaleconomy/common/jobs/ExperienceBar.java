0c305b264b24885c0a916e134843523a6caa5190
Eric Grandt <3956301+ericgrandt@users.noreply.github.com>
2024-11-23 19:42:34 -0600

    Oops, fix

--- totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/jobs/ExperienceBar.java
+++ totaleconomy-common/src/main/java/com/ericgrandt/totaleconomy/common/jobs/ExperienceBar.java
@@ -42,7 +42,7 @@ public class ExperienceBar {
                 String.format(
                     "%s [+%s EXP]",
                     expBarDto.jobName(),
-                    expGain,
+                    expGain
                 )
             )
         );
