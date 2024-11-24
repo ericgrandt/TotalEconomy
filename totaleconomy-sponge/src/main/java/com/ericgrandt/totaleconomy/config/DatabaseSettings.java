92d6068e5fc683cf98a4fefb39ad8fb2fa8eac00
Eric Grandt <3956301+ericgrandt@users.noreply.github.com>
2024-11-23 19:35:37 -0600

    Update Sponge to allow changing default configuration

--- totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/config/DatabaseSettings.java
+++ totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/config/DatabaseSettings.java
@@ -1,18 +1,12 @@
 package com.ericgrandt.totaleconomy.config;
 
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 @ConfigSerializable
 public class DatabaseSettings {
-    @Setting
-    private final String url = "jdbc:mysql://localhost:3306/totaleconomy";
-
-    @Setting
-    private final String user = "root";
-
-    @Setting
-    private final String password = "password";
+    private String url = "jdbc:mysql://localhost:3306/totaleconomy";
+    private String user = "root";
+    private String password = "password";
 
     public String getUrl() {
         return url;
--- totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/config/FeatureSettings.java
+++ totaleconomy-sponge/src/main/java/com/ericgrandt/totaleconomy/config/FeatureSettings.java
@@ -3,12 +3,10 @@ package com.ericgrandt.totaleconomy.config;
 import java.util.HashMap;
 import java.util.Map;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
-import org.spongepowered.configurate.objectmapping.meta.Setting;
 
 @ConfigSerializable
 public class FeatureSettings {
-    @Setting
-    private final boolean jobs = true;
+    private boolean jobs = false;
 
     public Map<String, Boolean> getFeatures() {
         Map<String, Boolean> features = new HashMap<>();
